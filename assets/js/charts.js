04bb7edcbb70927f6250cedd44e9a06e02c10390
Crabouille777 <crabouille777@gmail.com>
2025-07-27 19:10:49 +0200

    Style css de la courbe
    Style css de la courbe

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,6 +1,7 @@
 function createLineChart(canvasId, labels, data, label) {
     const ctx = document.getElementById(canvasId);
     if (!ctx) return;
+
     new Chart(ctx, {
         type: 'line',
         data: {
@@ -8,11 +9,12 @@ function createLineChart(canvasId, labels, data, label) {
             datasets: [{
                 label: label,
                 data: data,
-                fill: false,
-                borderColor: '#10B981',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                fill: true,
                 tension: 0.3,
-                pointRadius: 4,
-                pointBackgroundColor: '#10B981'
+                pointRadius: 3,
+                //pointBackgroundColor: 'rgba(75, 192, 192, 1)'
             }]
         },
         options: {
